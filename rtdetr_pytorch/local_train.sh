a773768b2314ff66b84816b28539423a799f16fb
wangfengguo <wangfengguo@shizhuang-inc.com>
2025-05-06 18:35:07 +0800

    feat: update train sh

--- rtdetr_pytorch/local_train.sh
+++ rtdetr_pytorch/local_train.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 
+rm -rf /root/RT-DETR/rtdetr_pytorch/configs/dataset/dfui
+ln -s /root/autodl-tmp/dfui /root/RT-DETR/rtdetr_pytorch/configs/dataset
+
 CONFIG_FILE=${1}
-python tools/train.py -c $CONFIG_FILE -t checkpoints/rtdetr_r18vd_dec3_6x_coco_from_paddle.pth --seed 42
+python tools/train.py -c configs/rtdetr/$CONFIG_FILE -t checkpoints/rtdetr_r18vd_dec3_6x_coco_from_paddle.pth --seed 42
